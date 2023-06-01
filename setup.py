1fe9869419083e64f71171fda1579f04f827274f
David Schmid <david.schmid@mailbox.org>
2023-06-01 11:42:27 +0200

    Update test and Refactor LatticePy functionalities
    - Rename the 'transform_basis_with_unimodular' function to 'transform_basis'
      in lattices.py.
    - Rename the 'compute_lattice_determinant' function to 'lattice_determinant'
      in lattices.py.
    - Improve code consistency and readability by adhering to PEP 8 standards,
      specifically in terms of spacing and line wrapping.
    - Update test cases in tests/test_lattices.py with
      test_lattice_determinant
    
    These changes aim to improve the overall clarity of the codebase and ensure adherence to Python's PEP 8 style guide.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author="David Schmid",
     author_email="david.schmid@mailbox.org",
     description="A Python library for working with lattices",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="http://github.com/5n00py/LatticePy",
     classifiers=[
         "License :: OSI Approved :: MIT License",
--- tests/test_lattices.py
+++ tests/test_lattices.py
@@ -3,13 +3,21 @@ import numpy as np
 
 from LatticePy import lattices
 
+
 def test_compute_gram_matrix() -> None:
     # Test vector from Bremner, 2012, Example 1.16
-    X = np.array([[-7, -7, 4, -8, -8],
-                  [1, 6, -5, 8, -1],
-                  [-1, 1, 4, -7, 8]])
+    X = np.array([[-7, -7, 4, -8, -8], [1, 6, -5, 8, -1], [-1, 1, 4, -7, 8]])
     GM = lattices.compute_gram_matrix(X)
-    E = np.array([[242, -125, 8],
-                  [-125, 127, -79],
-                  [8, -79, 131]])
+    E = np.array([[242, -125, 8], [-125, 127, -79], [8, -79, 131]])
     assert np.array_equal(GM, E)
+
+
+def test_lattice_determinant() -> None:
+    # Test vector from Bremner, 2012, Example 1.16
+    X = np.array([[-7, -7, 4, -8, -8], [1, 6, -5, 8, -1], [-1, 1, 4, -7, 8]])
+    det = lattices.lattice_determinant(X)
+
+    # Note: The tolerance is relative to the expected value and is 1e-6
+    # (i.e., can differ by up to 1e-6 * expected_value), but this could be
+    # changed by passing a different value to pytest.approx if needed...
+    assert det == pytest.approx(np.sqrt(618829))
